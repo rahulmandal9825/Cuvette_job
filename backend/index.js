5130adc974f540df3789e8b72cb53e3f134234bc
rahulmandal9825 <rahulmandalzzz123@gmail.com>
2024-10-20 12:42:30 +0530

    productions

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/index.js";
-import authRouters from "./routes/auth.route.js"
-import interviewformroutes from "./routes/interviewform.route.js"
+import authRouters from "./routes/auth.route.js";
+import interviewformroutes from "./routes/interviewform.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
+// Fix for __dirname in ES modules
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -14,13 +18,18 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use(express.json()); // allows us to parse incoming requests:req.body
-app.use(cookieParser()); // allows us to parse incoming cookies
+app.use(express.json()); // Allows us to parse incoming requests: req.body
+app.use(cookieParser()); // Allows us to parse incoming cookies
+
+app.use("/api/auth", authRouters);
+app.use("/api/form", interviewformroutes);
 
 
-app.use("/api/auth", authRouters)
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.use("/api/form", interviewformroutes)
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 
 app.listen(PORT, () => {
 	connectDB();
